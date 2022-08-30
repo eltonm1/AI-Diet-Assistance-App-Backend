65141e6906f95afa2a7517e8ff618ec8f81f5fc2
Elton Miao <eltonmiao@hotmail.com>
2022-08-30 19:37:54 +0800

    Add unit-test for users

--- AI_Diet_Assistance/user_manager/tests.py
+++ AI_Diet_Assistance/user_manager/tests.py
@@ -1,4 +1,35 @@
 from django.test import TestCase
-
+from rest_framework.test import APIRequestFactory
 # Create your tests here.
+from .models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import force_authenticate
+from .views import UserViewList
 
+class UserTests(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(
+                **{
+                    "email" : "test_email@test.com",
+                    "date_of_birth" : "2000-01-01",
+                    },
+        )
+        user.set_password("123")
+        user.save()
+        self.user = user
+        self.token = Token.objects.create(user=user)
+
+    def test_get_all_users_authenticated(self):
+        factory = APIRequestFactory()
+        request = factory.get('/users/')
+
+        force_authenticate(request, user=self.user, token=self.user.auth_token)
+        response = UserViewList.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_all_users_not_authenticated(self):
+        factory = APIRequestFactory()
+        request = factory.get('/users/')
+
+        response = UserViewList.as_view()(request)
+        self.assertEqual(response.status_code, 200)
